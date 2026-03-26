c42bcd69a28a72891c3c62561124ba7be243940b
Ocholobos <carlos8a10@yahoo.com>
2026-03-25 21:44:46 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // ============================================================
 // DATOS CENTRALIZADOS DE EPISODIOS TRADUCIDOS
-// Última actualización: 25/03/2026 09:56
+// Última actualización: 25/03/2026 21:42
 // ============================================================
 
 const GACHA_DATA = {
@@ -245,6 +245,308 @@ const GACHA_DATA = {
                 'num': 10,
                 'title': 'Siguiente acción',
                 'date': '2020/05/07 17:00'
+            },
+            {
+                'num': 11,
+                'title': 'Rey Leaf VII',
+                'date': '2020/05/08 12:00'
+            },
+            {
+                'num': 12,
+                'title': 'Flecha del Trueno',
+                'date': '2020/05/08 17:00'
+            },
+            {
+                'num': 13,
+                'title': 'Las intenciones de cada uno',
+                'date': '2020/05/09 12:00'
+            },
+            {
+                'num': 14,
+                'title': 'Después de casi tres años...',
+                'date': '2020/05/09 17:00'
+            },
+            {
+                'num': 15,
+                'title': 'Conversación con Sasha',
+                'date': '2020/05/10 12:00'
+            },
+            {
+                'num': 16,
+                'title': 'Tres personas',
+                'date': '2020/05/10 17:00'
+            },
+            {
+                'num': 17,
+                'title': 'Heathel y Suz',
+                'date': '2020/05/11 12:00'
+            },
+            {
+                'num': 18,
+                'title': 'El objetivo',
+                'date': '2020/05/11 17:00'
+            },
+            {
+                'num': 19,
+                'title': 'Nivel 7777',
+                'date': '2020/05/12 12:00'
+            },
+            {
+                'num': 20,
+                'title': 'Sasha - hacia el bosque',
+                'date': '2020/05/12 17:00'
+            },
+            {
+                'num': 21,
+                'title': 'Sasha - de reconocimiento',
+                'date': '2020/05/13 12:00'
+            },
+            {
+                'num': 22,
+                'title': 'La Orden de los Caballeros Blancos decide salir',
+                'date': '2020/05/13 17:00'
+            },
+            {
+                'num': 23,
+                'title': 'Reunión de estrategia',
+                'date': '2020/05/14 12:00'
+            },
+            {
+                'num': 24,
+                'title': 'Preparativos de cada uno para la entrada',
+                'date': '2020/05/14 17:00'
+            },
+            {
+                'num': 25,
+                'title': 'Entrada a la torre gigante',
+                'date': '2020/05/15 12:00'
+            },
+            {
+                'num': 26,
+                'title': 'Batalla en el primer piso 1',
+                'date': '2020/05/15 17:00'
+            },
+            {
+                'num': 27,
+                'title': 'Batalla en el primer piso 2',
+                'date': '2020/05/16 12:00'
+            },
+            {
+                'num': 28,
+                'title': 'Batalla en el primer piso 3',
+                'date': '2020/05/16 17:00'
+            },
+            {
+                'num': 29,
+                'title': 'Batalla en el segundo piso 1',
+                'date': '2020/05/17 12:00'
+            },
+            {
+                'num': 30,
+                'title': 'Batalla en el segundo piso 2',
+                'date': '2020/05/17 17:00'
+            },
+            {
+                'num': 31,
+                'title': 'Batalla en el segundo piso 3',
+                'date': '2020/05/18 12:00'
+            },
+            {
+                'num': 32,
+                'title': 'Batalla en el tercer piso 1',
+                'date': '2020/05/18 17:00'
+            },
+            {
+                'num': 33,
+                'title': 'Batalla en el tercer piso 2',
+                'date': '2020/05/19 12:00'
+            },
+            {
+                'num': 34,
+                'title': 'Reencuentro después de casi tres años',
+                'date': '2020/05/19 17:00'
+            },
+            {
+                'num': 35,
+                'title': 'Encuentro después de casi tres años',
+                'date': '2020/05/20 12:00'
+            },
+            {
+                'num': 36,
+                'title': 'La segunda venganza',
+                'date': '2020/05/20 17:00'
+            },
+            {
+                'num': 37,
+                'title': '¿Qué es la desesperación?',
+                'date': '2020/05/21 12:00'
+            },
+            {
+                'num': 38,
+                'title': 'Enterrador Divino Gungnir - Primera fase',
+                'date': '2020/05/21 17:00'
+            },
+            {
+                'num': 39,
+                'title': 'La carta de triunfo de Michael',
+                'date': '2020/05/22 12:00'
+            },
+            {
+                'num': 40,
+                'title': 'Disputa',
+                'date': '2020/05/22 17:00'
+            },
+            {
+                'num': 41,
+                'title': 'Ataduras de espinas',
+                'date': '2020/05/23 12:00'
+            },
+            {
+                'num': 42,
+                'title': 'Venganza contra Sasha',
+                'date': '2020/05/23 17:00'
+            },
+            {
+                'num': 43,
+                'title': 'Nueva información y caída de un país',
+                'date': '2020/05/24 12:00'
+            },
+            {
+                'num': 44,
+                'title': 'La reunión danzante',
+                'date': '2020/05/24 17:00'
+            },
+            {
+                'num': 45,
+                'title': 'Recuerdos del Rey Leaf VII',
+                'date': '2020/05/25 12:00'
+            },
+            {
+                'num': 46,
+                'title': 'Declaración de Independencia Absoluta de la Raza Humana',
+                'date': '2020/05/25 17:00'
+            },
+            {
+                'num': 47,
+                'title': 'Nueva información y --',
+                'date': '2020/05/26 12:00'
+            },
+            {
+                'num': 48,
+                'title': 'Extra: Pasta de frijol dulce o con grano',
+                'date': '2020/05/26 17:00'
+            },
+            {
+                'num': 49,
+                'title': 'Extra: El baño y el secreto',
+                'date': '2020/05/27 12:00'
+            },
+            {
+                'num': 50,
+                'title': 'Extra: La historia de Nemumu',
+                'date': '2020/05/27 17:00'
+            }
+        ]
+    },
+    {
+        'num': 3,
+        'title': 'El pasado de Light',
+        'episodes': [
+            {
+                'num': 1,
+                'title': 'Light - nivel 15',
+                'date': '2020/05/28 12:00'
+            },
+            {
+                'num': 2,
+                'title': 'Light - nivel 1000',
+                'date': '2020/05/28 17:00'
+            },
+            {
+                'num': 3,
+                'title': 'Capítulo del baño',
+                'date': '2020/05/29 12:00'
+            },
+            {
+                'num': 4,
+                'title': 'Light - nivel alrededor de 3000',
+                'date': '2020/05/29 17:00'
+            },
+            {
+                'num': 5,
+                'title': 'Light - nivel alrededor de 3000 (2)',
+                'date': '2020/05/30 12:00'
+            },
+            {
+                'num': 6,
+                'title': 'vs Hydra',
+                'date': '2020/05/30 17:00'
+            },
+            {
+                'num': 7,
+                'title': 'Light - nivel 3500',
+                'date': '2020/05/31 12:00'
+            },
+            {
+                'num': 8,
+                'title': 'Calabozo \'Abismo\' - Primer año',
+                'date': '2020/05/31 17:00'
+            },
+            {
+                'num': 9,
+                'title': 'Light - nivel 7000',
+                'date': '2020/06/01 12:00'
+            },
+            {
+                'num': 10,
+                'title': 'Calabozo \'Abismo\' - Segundo año',
+                'date': '2020/06/01 17:00'
+            },
+            {
+                'num': 11,
+                'title': 'Regreso a casa 1',
+                'date': '2020/06/02 12:00'
+            },
+            {
+                'num': 12,
+                'title': 'Regreso a casa 2',
+                'date': '2020/06/02 17:00'
+            },
+            {
+                'num': 13,
+                'title': 'Extra 1',
+                'date': '2020/06/03 12:00'
+            },
+            {
+                'num': 14,
+                'title': 'Extra 2',
+                'date': '2020/06/03 17:00'
+            }
+        ]
+    },
+    {
+        'num': 4,
+        'title': 'El reino humano',
+        'episodes': [
+            {
+                'num': 1,
+                'title': 'Justicia azul',
+                'date': '2020/06/04 12:00'
+            },
+            {
+                'num': 2,
+                'title': 'El paradero de Yume',
+                'date': '2020/06/04 17:00'
+            },
+            {
+                'num': 3,
+                'title': 'Hacia la torre gigante - Inspección',
+                'date': '2020/06/05 12:00'
+            },
+            {
+                'num': 4,
+                'title': 'Emoción del reencuentro',
+                'date': '2020/06/05 17:00'
             }
         ]
     }
